5a3e9dc6a0a1a27f87a68f27aa079d76734a62ec
WV <WV@DESKTOP-V06234D>
2020-08-16 16:58:33 +0200

    delete forward slash

--- Application/demo-app-spring/src/main/java/com/novi/DiabloDemoDrop/utility/JavaMailUtil.java
+++ Application/demo-app-spring/src/main/java/com/novi/DiabloDemoDrop/utility/JavaMailUtil.java
@@ -10,7 +10,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-/
+
 public class JavaMailUtil {
     
     public static void sendMail(String recepient) throws Exception {
@@ -22,7 +22,7 @@ public class JavaMailUtil {
         properties.put("mail.smtp.host", "smtp.gmail.com");
         properties.put("mail.smtp.port", "587");
         
-        //real account credentials not in ublic repository
+        //real account credentials not in public repository
         String myAccountEmail = "dummy";
         String password = "dummy";
                 
